db74c4703e08fd54a6819d7aa79dfc9ea784a705
Antonio Lorenzo <115792915+antoniolv88@users.noreply.github.com>
2024-03-15 12:59:58 +0100

    Create principal.py

---
+++ crear_modulo/principal.py
@@ -0,0 +1,9 @@
+# principal.py
+#llamamos al modulo que hemos creado
+import operaciones
+
+resultado_suma = operaciones.suma(5, 3)
+print("Suma:", resultado_suma)
+
+resultado_resta = operaciones.resta(10, 4)
+print("Resta:", resultado_resta)
