a65e4bbfde3f8f54c79fb41fe7420ae2214752f4
Antonio Lorenzo <115792915+antoniolv88@users.noreply.github.com>
2024-03-15 09:46:46 +0100

    Create operadores_aritméticos_y_de_comparacion.py

---
+++ operadores_aritméticos_y_de_comparacion.py
@@ -0,0 +1,39 @@
+# Operadores aritméticos
+num1 = 10
+num2 = 3
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+division_entera = num1 // num2
+resto = num1 % num2
+exponenciacion = num1 ** num2
+
+print("Operadores aritméticos:")
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("División Entera:", division_entera)
+print("Resto:", resto)
+print("Exponenciación:", exponenciacion)
+
+# Operadores de comparación
+valor1 = 10
+valor2 = 20
+
+igualdad = valor1 == valor2
+desigualdad = valor1 != valor2
+mayor_que = valor1 > valor2
+menor_que = valor1 < valor2
+mayor_igual_que = valor1 >= valor2
+menor_igual_que = valor1 <= valor2
+
+print("\nOperadores de comparación:")
+print("Igualdad:", igualdad)
+print("Desigualdad:", desigualdad)
+print("Mayor que:", mayor_que)
+print("Menor que:", menor_que)
+print("Mayor o igual que:", mayor_igual_que)
+print("Menor o igual que:", menor_igual_que)
