d7560f421d9660f5c6c992debcedb6c4366bfe19
Antonio Lorenzo <115792915+antoniolv88@users.noreply.github.com>
2024-03-15 12:52:05 +0100

    Create importar_modulos.py

---
+++ importar_modulos.py
@@ -0,0 +1,9 @@
+# Importar el módulo math
+import math
+
+# Utilizar la función sqrt() para calcular la raíz cuadrada
+numero = 16
+raiz_cuadrada = math.sqrt(numero)
+
+# Imprimir el resultado
+print("La raíz cuadrada de", numero, "es:", raiz_cuadrada)
